9ad1a164ebef869a3d1c864b347824287044af8d
Arkadiusz Górka <arekgorka91@gmail.com>
2023-04-03 00:05:08 +0200

    CryptoService tests

--- src/test/java/com/kantor/service/CryptoServiceTests.java
+++ src/test/java/com/kantor/service/CryptoServiceTests.java
@@ -1,6 +1,62 @@
 package com.kantor.service;
 
+import com.kantor.domain.currency.Bitcoin;
+import com.kantor.exception.CurrencyNotFoundException;
+import com.kantor.repository.BitcoinRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class CryptoServiceTests {
 
-    //to do
+    private final Bitcoin bitcoin = new Bitcoin(41000.3544,40500.3544,"BTC");
+
+    @Autowired
+    private CryptoService cryptoService;
+    @Autowired
+    private BitcoinRepository bitcoinRepository;
+
+    @Test
+    void getCryptoBuyFromRepositoryTest() throws CurrencyNotFoundException {
+        //Given
+        bitcoinRepository.save(bitcoin);
+        //When
+        double btc = cryptoService.getCryptoBuyFromRepository("bitcoin");
+        //Then
+        assertEquals(41000.3544, btc);
+    }
+
+    @Test
+    void getCryptoBuyWithWrongCurrencyTest() {
+        //Given
+        //When & Then
+        assertThrows(
+                CurrencyNotFoundException.class,
+                ()->cryptoService.getCryptoBuyFromRepository(""),
+                "Wrong currency");
+    }
+
+    @Test
+    void getCryptoSellFromRepositoryTest() throws CurrencyNotFoundException {
+        //Given
+        bitcoinRepository.save(bitcoin);
+        //When
+        double btc = cryptoService.getCryptoSellFromRepository("bitcoin");
+        //Then
+        assertEquals(41000.3544, btc);
+    }
+
+    @Test
+    void getCryptoSellWithWrongCurrencyTest() {
+        //Given
+        //When & Then
+        assertThrows(
+                CurrencyNotFoundException.class,
+                ()->cryptoService.getCryptoSellFromRepository(""),
+                "Wrong currency");
+    }
+
 }
